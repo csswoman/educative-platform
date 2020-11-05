1b21400544d29ea3e2e0b4bb991a56616b112576
Karla Agraz <karla.agraz@gmail.com>
2020-11-05 00:54:42 -0500

    Actualizar la cantidad de Elementos si el elemento ya existe

--- js/app.js
+++ js/app.js
@@ -33,8 +33,23 @@ function leerDatosCurso(curso) {
     cantidad: 1,
   };
 
-  // Agrega elementos al arreglo de carrito
-  articulosCarrito = [...articulosCarrito, infoCurso];
+  //Revisa si un elemento ya existe en el carrito
+  const existe = articulosCarrito.some((curso) => curso.id === infoCurso.id);
+  if (existe) {
+    //Actualizamos la cantidad
+    const cursos = articulosCarrito.map((curso) => {
+      if (curso.id === infoCurso.id) {
+        curso.cantidad++;
+        return curso; // retorna el objeto actualizado
+      } else {
+        return curso; // retorna los objetos que no son duplicados
+      }
+    });
+    articulosCarrito = [...cursos];
+  } else {
+    // Agrega elementos al arreglo de carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+  }
 
   console.log(articulosCarrito);
 
