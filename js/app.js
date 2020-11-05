02f3efd818a07b5e3169ab40cbff5442c958f91d
Karla Agraz <karla.agraz@gmail.com>
2020-11-05 00:46:21 -0500

    mostrando el resto de la información

--- js/app.js
+++ js/app.js
@@ -48,10 +48,17 @@ function carritoHTML() {
 
   //Recorre el carrito y genera el HTML
   articulosCarrito.forEach((curso) => {
+    const { imagen, titulo, precio, cantidad, id } = curso;
     const row = document.createElement("tr");
     row.innerHTML = `
       <td>
-      ${curso.titulo}
+        <img src="${imagen}" width="100">
+      </td>
+      <td>${titulo}</td>
+      <td>${precio}</td>
+      <td>${cantidad}</td>
+      <td>
+      <a href="#" class="borrar-curso" data-id="${id}"> X </a>
       </td>
     `;
 
